a92e290d5fc40444826e4b5a448ef533cd55417c
Draj19071997 <draj.shet45@gmail.com>
2025-02-16 23:13:04 +0530

    Comitted

--- src/main/java/drivers/DriverManager.java
+++ src/main/java/drivers/DriverManager.java
@@ -1,8 +1,25 @@
 package drivers;
 
-public class DriverManager {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
 
+public class DriverManager {
+	private static final Logger logger = LogManager.getLogger(DriverManager.class);
 	private DriverManager() {};
-	
-	
+	private static ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
+	private static DriverManager DriverManagerInstance;
+
+	//Getter method to access the private DriverManagerInstance
+	public static DriverManager getinstance() {
+		if (DriverManagerInstance == null) {
+			synchronized (DriverManager.class) {
+				if (DriverManagerInstance == null) {
+					DriverManagerInstance = new DriverManager();
+				}
+			}
+		}
+		return DriverManagerInstance;
+	}
+
 }
