0f7a714e7837506c05b87862e57cd5eda4a4ff71
Draj19071997 <draj.shet45@gmail.com>
2025-02-17 00:08:19 +0530

    added some code in driver package

--- src/main/java/utils/Log4jUtils.java
+++ src/main/java/utils/Log4jUtils.java
@@ -1,5 +1,38 @@
 package utils;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class Log4jUtils {
 
+	private static Logger logger;
+
+	private Log4jUtils() {
+		// Private constructor to prevent instantiation
+	}
+
+	public static Logger getLogger(Class<?> className) {
+		if (logger == null) {
+			logger = LogManager.getLogger(className);
+		}
+		return logger;
+	}
+
+	public static void info(String message) {
+		logger.info(message);
+	}
+
+	public static void warn(String message) {
+		logger.warn(message);
+	}
+
+	public static void error(String message) {
+		logger.error(message);
+	}
+
+	public static void debug(String message) {
+		logger.debug(message);
+	}
 }
